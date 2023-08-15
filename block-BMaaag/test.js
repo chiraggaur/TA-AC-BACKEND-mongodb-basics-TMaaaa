529de1dbbd6d2b0360d535ab4fa3f7d4132966b4
chirag gaur <chiragwalker@gmail.com>
2023-08-15 20:24:09 +0530

    done

---
+++ block-BMaaag/test.js
@@ -0,0 +1,6 @@
+var str = [
+  { name: "Dhauldhar range", height: "4000 mtrs" },
+  { name: "Dham range", height: "4000 mtrs" },
+  { name: "abhshiek", height: "4000 mtrs" },
+  { name: "stunt", height: "4000 mtrs" },
+];
